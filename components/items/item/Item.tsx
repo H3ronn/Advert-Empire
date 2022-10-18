47fbeb34e8dea4dfb275d8900c74bc441f7b8a3a
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-10-18 21:10:50 +0200

    feat: add placeholder when item has no image

--- components/items/item/Item.tsx
+++ components/items/item/Item.tsx
@@ -1,4 +1,5 @@
 import type { Item as ItemType } from '../../../types/item';
+import NoImage from '../noImage/NoImage';
 import styles from './item.module.scss';
 
 type ItemProps = {
@@ -8,11 +9,13 @@ type ItemProps = {
 const Item = ({ itemData: { image, name, locality, price, added } }: ItemProps) => {
   return (
     <li className={styles.wrapper}>
-      <img className={styles.image} src={image} alt="" />
-      <div className={styles.name}>{name}</div>
-      <div className={styles.locality}>{locality}</div>
-      <div className={styles.price}>{price} zł</div>
-      <div className={styles.added}>{added}</div>
+      {image ? <img className={styles.image} src={image} alt="" /> : <NoImage />}
+      <div>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.locality}>{locality}</div>
+        <div className={styles.price}>{price} zł</div>
+        <div className={styles.added}>{added}</div>
+      </div>
     </li>
   );
 };
---
+++ components/items/noImage/NoImage.tsx
@@ -0,0 +1,7 @@
+import styles from './noImage.module.scss';
+
+const NoImage = () => {
+  return <div className={styles.wrapper}>No Image</div>;
+};
+
+export default NoImage;
